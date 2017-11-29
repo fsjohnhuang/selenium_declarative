f7bedf2616493922fcff8f5b4129eadcd4e8b0ba
fsjohnhuang <fsjohnhuang@hotmail.com>
2017-11-29 13:43:04 +0800

    add new guy

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium_declarative.test_runner.core import TestRunner
+
+def main():
+    driver = webdriver.Chrome()
+    t = TestRunner(driver, "http://gqms.midea.com", "/home/john/selenium_declarative/test", "/home/john/sd.test")
+    t.run()
+main()
